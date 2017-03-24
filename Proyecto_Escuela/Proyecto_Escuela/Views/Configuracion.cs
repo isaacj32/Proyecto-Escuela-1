b079abea1ffd69801efa46042d42fa0c450589c8
Apa <Apa@DESKTOP-VR3MEFU>
2017-03-24 12:29:59 -0500

    merge

--- Proyecto_Escuela/Proyecto_Escuela/Views/Configuracion.cs
+++ Proyecto_Escuela/Proyecto_Escuela/Views/Configuracion.cs
@@ -35,11 +35,5 @@ namespace Proyecto_Escuela.Views
             new ConfigurarTexto().Show();
             this.Dispose();
         }
-
-        private void jugadores_Click(object sender, EventArgs e)
-        {
-            new Estadistica().Show();
-            this.Dispose();
-        }
     }
 }
